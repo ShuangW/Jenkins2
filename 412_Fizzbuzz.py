8a43d5dc180ae47a0a3d1b393510af82c91d4f91
ShuangW <wangshuang06@gmail.com>
2018-07-12 10:13:16 -0700

    Update 412_Fizzbuzz.py

--- 412_Fizzbuzz.py
+++ 412_Fizzbuzz.py
@@ -27,3 +27,21 @@ Return:
 
 """
 
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ls_fizzbuzz=[]
+        for i in range(1,n+1):
+            if i%3 == 0 and i%5 == 0:
+                ls_fizzbuzz+=['FizzBuzz']
+            elif i%3 == 0:
+                ls_fizzbuzz+=['Fizz']
+            elif i%5 == 0:
+                ls_fizzbuzz+=['Buzz']
+            else:
+                ls_fizzbuzz+= [str(i)]
+        return ls_fizzbuzz
+        
